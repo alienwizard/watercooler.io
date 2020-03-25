346c9e57a624cb069d36422518a77b6099882b1c
johanwen <johan.wendin@tele2.com>
2020-03-25 11:11:05 +0100

    Add movement to playerController

--- Assets/Scripts/PlayerCharacterController.cs
+++ Assets/Scripts/PlayerCharacterController.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerCharacterController : MonoBehaviour
 {
+    public float walkSpeed = 5.0F;
+    public float jumpHeight = 10.0F;
+    public float gravity = 20.0F;
+
+    private Vector3 moveDirection = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        handleMovement();
+    }
+
+    private void handleMovement()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= walkSpeed;
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpHeight;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
