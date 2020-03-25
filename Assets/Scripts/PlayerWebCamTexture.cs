00fd67d7f640e207a3cb4f06a32eab9dfbd46bd5
johanwen <johan.wendin@tele2.com>
2020-03-25 10:27:35 +0100

    Initial stuff

---
+++ Assets/Scripts/PlayerWebCamTexture.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWebCamTexture : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        GetComponent<Renderer>().material.mainTexture = webcamTexture;
+        // webcamTexture.Play();
+    }
+
+    private void Update()
+    {
+
+    }
+
+}
