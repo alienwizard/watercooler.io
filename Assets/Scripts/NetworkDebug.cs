2db5c82f328362fed337f16e56d16d10c9abeefd
johanwen <johan.wendin@tele2.com>
2020-03-26 15:28:16 +0100

    WIP

--- Assets/Scripts/NetworkDebug.cs
+++ Assets/Scripts/NetworkDebug.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 using Mirror;
 
 
-public class NetworkDebug : MonoBehaviour
+public class NetworkDebug : NetworkBehaviour
 {
     // Start is called before the first frame update
     void OnGUI()
     {
         string ipadress = NetworkClient.serverIp;
+        GUI.Box(new Rect(10, Screen.height - 75, 100, 50), "isLocalPlayer: " + isLocalPlayer);
         GUI.Box(new Rect(10, Screen.height - 50, 100, 50), ipadress);
         GUI.Label(new Rect(20, Screen.height - 35, 100, 20), "Status: " + NetworkServer.active);
         GUI.Label(new Rect(20, Screen.height - 20, 100, 20), "Connected: " + NetworkServer.connections.Count);
--- Assets/Scripts/PlayerWebCamTexture.cs
+++ Assets/Scripts/PlayerWebCamTexture.cs
@@ -6,30 +6,75 @@ using Mirror;
 
 public class PlayerWebCamTexture : NetworkBehaviour
 {
-    public WebCamTexture webcamTexture;
     public Quaternion baseRotation;
+    private WebCamTexture webcamTexture;
+    private GameObject head;
     // Start is called before the first frame update
     void Start()
     {
-        NetworkIdentity identity = GetComponentInParent<NetworkIdentity>();
-        if (identity.isLocalPlayer)
+        if (!isLocalPlayer)
         {
-            webcamTexture = new WebCamTexture();
-            GetComponent<Renderer>().material.mainTexture = webcamTexture;
-            baseRotation = transform.rotation;
-            webcamTexture.requestedFPS = 30f;
-
-            if (!Application.isEditor)
-            {
-                webcamTexture.Play();
-            }
+            return;
         }
+
+        head = GameObject.Find("Head");
+        Debug.Log("Start");
+
+
+        webcamTexture = new WebCamTexture();
+        head.GetComponent<Renderer>().material.mainTexture = webcamTexture;
+        baseRotation = transform.rotation;
+        webcamTexture.requestedFPS = 30f;
+
+        if (!Application.isEditor)
+        {
+            webcamTexture.Play();
+        }
+
 
     }
 
-    private void Update()
+    public override void OnStartLocalPlayer()
     {
-        transform.rotation = baseRotation * Quaternion.AngleAxis(webcamTexture.videoRotationAngle, Vector3.up);
+        head = GameObject.Find("Head");
+
+
+        webcamTexture = new WebCamTexture();
+        head.GetComponent<Renderer>().material.mainTexture = webcamTexture;
+        baseRotation = transform.rotation;
+        webcamTexture.requestedFPS = 30f;
+
+        if (!Application.isEditor)
+        {
+            webcamTexture.Play();
+        }
+    }
+
+    void OnConnectedToServer()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        head = GameObject.Find("Head");
+
+
+        webcamTexture = new WebCamTexture();
+        head.GetComponent<Renderer>().material.mainTexture = webcamTexture;
+        baseRotation = transform.rotation;
+        webcamTexture.requestedFPS = 30f;
+
+        if (!Application.isEditor)
+        {
+            webcamTexture.Play();
+        }
+    }
+
+
+    void OnServerInitialized()
+    {
+        Debug.Log("Server initialized and ready");
     }
 
 }
